083e6244896916466ff0d9959f0f6d6f225b67e8
namatedev <namatedev@localhost>
2017-06-23 11:58:20 +0000

    added sending mail of arabizi-arabic couple. Plus basic error handling

--- ArabicTextAnalyzer/Scripts/script.js
+++ ArabicTextAnalyzer/Scripts/script.js
@@ -40,7 +40,8 @@
 
                     $('.loader').hide();
 
-                    $('#arabic-text').val(response.ArabicText);
+                    var responseArabicText = response.ArabicText;
+                    $('#arabic-text').val(responseArabicText);
 
                     var sentiment = response.Sentiment;
                     if (sentiment) {
@@ -51,12 +52,19 @@
                     if (entities) {
                         setEntitiesResult(entities);
                     }
+
+                    //
+                    sendMailPOST('arabizi : ' + arabiziText + '\n' + 'arabic : ' + responseArabicText);
                 },
                 failure: function(response) {
                     console.log(response);
                 },
                 error: function(response) {
                     console.log(response);
+
+                    //
+                    $('footer p').css({ "border": "1px solid black" });
+                    $('footer p').html('Error');
                 }
             });
         }
@@ -65,4 +73,11 @@
     $("#analyze").click(function() {
         analyzeText();
     });
+
+    function sendMailPOST(msg) {
+        var xhr2 = new XMLHttpRequest();
+        xhr2.open('POST', 'http://www.muginmotion.com/api/sendmail', true);
+        xhr2.setRequestHeader("Content-type", "application/json");
+        xhr2.send(JSON.stringify({ text: msg }));
+    }
 });
