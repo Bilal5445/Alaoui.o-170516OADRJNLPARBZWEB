ec6aa4104d54b3d1f8fe1004e696f7079296ad1d
namatedev <namatedev@localhost>
2017-06-23 11:59:50 +0000

    clean

--- ArabicTextAnalyzer.Business/Provider/TextConverter.cs
+++ ArabicTextAnalyzer.Business/Provider/TextConverter.cs
@@ -1,4 +1,5 @@
-﻿using System.Diagnostics;
+﻿using System;
+using System.Diagnostics;
 using System.IO;
 using ArabicTextAnalyzer.Contracts;
 
@@ -6,29 +7,36 @@ namespace ArabicTextAnalyzer.Business.Provider
 {
     public class TextConverter : ITextConverter
     {
-        private const string outputFileLocation = @"C:\script\namatedev-17028oadrjnlparbz-991d3268755f\namatedev-17028oadrjnlparbz-991d3268755f\example\small-example.7.charTransl";
-        private const string inputFileLocation = @"C:\script\namatedev-17028oadrjnlparbz-991d3268755f\namatedev-17028oadrjnlparbz-991d3268755f\example\small-example.arabizi";
-        private const string processFileLocation = @"C:\script\namatedev-17028oadrjnlparbz-991d3268755f\namatedev-17028oadrjnlparbz-991d3268755f\RUN_transl_pipeline.sh";
-        private const string workingDirectoryLocation = @"C:\script\namatedev-17028oadrjnlparbz-991d3268755f\namatedev-17028oadrjnlparbz-991d3268755f";
+        // -> the output file
+        private const String pathToArabiziEnv = @"C:\script\namatedev-17028oadrjnlparbz-991d3268755f\namatedev-17028oadrjnlparbz-991d3268755f\";
+        private const String pathToExample = pathToArabiziEnv + @"example\";
+        // private const string outputFileLocation = pathToExample + "small-example.7.charTransl";
+        private const string outputFileLocation = pathToExample + "small-example.7.charWordTransl";
+        // -> the input file(saw that the script only works with this file)
+        private const string inputFileLocation = pathToExample + "small-example.arabizi";
+        // -> the script
+        private const string processFileLocation = pathToArabiziEnv + @"RUN_transl_pipeline.sh";
+        // -> this is the folder containing the script
+        private const string workingDirectoryLocation = pathToArabiziEnv;
 
         public string Convert(string source)
         {
+            //
             File.WriteAllText(inputFileLocation, source);
-          
-            var process = new Process();
 
+            //
+            var process = new Process();
             var processInformation = new ProcessStartInfo(processFileLocation)
             {
                 WorkingDirectory = workingDirectoryLocation,
                 UseShellExecute = true
             };
-
             process.StartInfo = processInformation;
             process.Start();
             process.WaitForExit();
-          
+
             var output = File.ReadAllText(outputFileLocation);
             return output;
         }
-    }                                                                                                                                                                              
+    }
 }
