ba69a9e4cfc4040f33f057cc21929995ab1767c9
namatedev <namatedev@localhost>
2017-09-19 14:01:15 +0100

    arabiziweb>ner>ui : adding search/highlight keywords

---
+++ ArabicTextAnalyzer/Scripts/mysite_train_keywordfiltering.js
@@ -0,0 +1,14 @@
+﻿window.onload = function () {
+    $(function () {
+
+        // Initialize DataTables
+        $('.datatables-table').DataTable({
+            // Enable mark.js search term highlighting
+            mark: true,
+            "drawCallback": function( settings ) {
+                alert( 'DataTables has redrawn the table' );
+            }
+        });
+
+    });
+}
