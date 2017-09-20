aee0380a0480fdd9d35d2a4c6978fb8d6efa41c5
namatedev <namatedev@localhost>
2017-09-20 12:40:18 +0100

    arabiziweb>ner>ui : adding ability to import many rows at once

--- ArabicTextAnalyzer/Controllers/TrainController.cs
+++ ArabicTextAnalyzer/Controllers/TrainController.cs
@@ -5,6 +5,7 @@ using ArabicTextAnalyzer.Models;
 using ArabicTextAnalyzer.ViewModels;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text.RegularExpressions;
 using System.Web;
@@ -455,6 +456,24 @@ namespace ArabicTextAnalyzer.Controllers
             }
 
             //
+            return RedirectToAction("Index");
+        }
+
+        // This action handles the form POST and the upload
+        [HttpPost]
+        public ActionResult Data_Upload(HttpPostedFileBase file)
+        {
+            // Verify that the user selected a file
+            if (file != null && file.ContentLength > 0)
+            {
+                // extract only the filename
+                var fileName = Path.GetFileName(file.FileName);
+                // store the file inside ~/App_Data/uploads folder
+                var path = Path.Combine(Server.MapPath("~/App_Data/uploads"), fileName);
+                file.SaveAs(path);
+            }
+
+            // redirect back to the index action to show the form once again
             return RedirectToAction("Index");
         }
     }
