70a29bd39c6a2af95fe1bf3f84736e2db2871043
namatedev <namatedev@localhost>
2017-09-26 20:08:04 +0100

    lean

--- ArabicTextAnalyzer.Domain/TextEntity.cs
+++ ArabicTextAnalyzer.Domain/TextEntity.cs
@@ -3,13 +3,9 @@
     public class TextEntity
     {
         public long Count { get; set; }
-
         public string EntityId { get; set; }
-
         public string Mention { get; set; }
-
         public string Normalized { get; set; }
-
         public string Type { get; set; }
     }
 }
