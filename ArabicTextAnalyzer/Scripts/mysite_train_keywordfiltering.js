17c6698d68ccea6ba0f9a49aaa2cc58cf2e949d1
namatedev <namatedev@localhost>
2017-10-09 18:58:34 +0000

    arabiziweb>ner>ui : adding themes/entities : added ability to copy table data (ex : to paste in excel) : cosmetic bugfix : button, filter, .. in same line

--- ArabicTextAnalyzer/Scripts/mysite_train_keywordfiltering.js
+++ ArabicTextAnalyzer/Scripts/mysite_train_keywordfiltering.js
@@ -10,10 +10,19 @@
             },
             aaSorting: [],   // just use the sorting from the server
             "pageLength": 100,
-            dom: 'Bfrtip',
+            dom: "<'row'<'col-sm-3'B><'col-sm-3'l><'col-sm-6'f>>" +
+                "<'row'<'col-sm-12'tr>>" +
+                "<'row'<'col-sm-5'i><'col-sm-7'p>>",
             buttons: [
                 'copyHtml5',
             ],
+        });
+
+        $('.datatables-table tbody').on('click', 'tr', function () {
+            $(this).toggleClass('selected');
+
+            // change a href
+            alert($(this).find("td:eq(6)").find("a").attr("href"));
         });
     });
 }
