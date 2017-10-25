2fd44d8fed37119ac85df051cce83b45ea36fa78
namatedev <namatedev@localhost>
2017-10-25 17:22:53 +0000

    arabiziweb>ui : added ability to delete many entries at once by multi selecting rows

--- ArabicTextAnalyzer/Scripts/mysite_train_keywordfiltering.js
+++ ArabicTextAnalyzer/Scripts/mysite_train_keywordfiltering.js
@@ -25,45 +25,49 @@ function InitializeDataTables() {
             // select the row
             $(this).toggleClass('selected');
 
-            // find the guid of arabiz entry from the href
-            var hrefInnerId = $(this).find("td:eq(6)").find("a").attr("href").substring("/Train/Train_DeleteEntry?arabiziWordGuid=".length);
-            // console.log(hrefInnerId);
-
             // add/remove guid to array
             if ($(this).hasClass('selected')) {
+
+                // we select
+
+                // find the guid of arabiz entry from the href
+                var hrefInnerId = $(this).find("td:eq(6)").find("> a").attr("href").substring("/Train/Train_DeleteEntry?arabiziWordGuid=".length);
+
+                // add it to global array
                 selectedArabiziIds.push(hrefInnerId);
+
+                // save old id in backud
+                $(this).find("td:eq(6)").find("> a").attr('data-backhref', hrefInnerId);
+
             } else {
-                var index = selectedArabiziIds.indexOf(hrefInnerId);
-                if (index > -1) {
-                    selectedArabiziIds.splice(index, 1);
-                }
+
+                // we deselect
+
+                // find the guid of arabiz entry from the backup href
+                var hrefBackInnerId = $(this).find("td:eq(6)").find("> a").attr("data-backhref");
+
+                // drop it from global (we know it is there)
+                var index = selectedArabiziIds.indexOf(hrefBackInnerId);
+                // if (index > -1) {
+                selectedArabiziIds.splice(index, 1);
+                // }
+
+                // set new value href (from backup)
+                var newhref = "/Train/Train_DeleteEntry?arabiziWordGuids=" + hrefBackInnerId;
+                $(this).find("td:eq(6)").find("> a").attr("href", newhref);
+
+                // console.log($(this).find("td:eq(6)").find("> a").attr("href"));
+                // console.log($(this).attr("class"));
             }
 
-            // the guids
-            // console.log(selectedArabiziIds);
-
-            // hide buttons remove/apply_tags ?
-            // $('tr.selected td:last-child').not(':first').remove();
-            // $('tr.selected:first td:last-child').attr('rowspan', '2');
-
-            // change hr to gather list of ids
-            /*console.log($('tr.selected td:last-child').each(function (index) {
-                // console.log(index + ": " + $(this).find("a").attr("href"));
-                console.log(index + ": " + $(this).find("> a").attr("href"));
-            }));*/
-            // .find("a").attr("href"));
+            // loop over selected to concatenate the arabizi entries ids
             $('tr.selected td:last-child').each(function (index) {
-                // console.log(index + ": " + $(this).find("a").attr("href"));
-                // console.log(index + ": " + $(this).find("> a").attr("href"));
-
                 // new value href
-                // var newhref = "/Train/Train_DeleteEntries?arabiziWordGuid=" + encodeURIComponent(selectedArabiziIds.join());
                 var newhref = "/Train/Train_DeleteEntries?arabiziWordGuids=" + selectedArabiziIds.join();
-                // console.log(newhref);
 
                 // set new value
                 $(this).find("> a").attr("href", newhref);
-            })
+            });
         });
     });
 }
