42cae8280d66605da911de7368ef11016b6209f6
namatedev <namatedev@localhost>
2018-06-07 19:34:26 +0000

    1014:DC>FB : social search : display only limited excerpt even when no keyword : max 400 characters

--- ArabicTextAnalyzer/Controllers/TrainController.cs
+++ ArabicTextAnalyzer/Controllers/TrainController.cs
@@ -2384,7 +2384,9 @@ namespace ArabicTextAnalyzer.Controllers
                 {
                     id = c.id,
                     fk_influencer = c.fk_influencer,
-                    pt = String.IsNullOrWhiteSpace(searchValue) ? c.post_text : truncateBySent(c.post_text, searchValue, sentPrepend: 0, sentAppend: 0, viewMoreTag: "[...]", startTruncTag: "[...]", returnSourceIfKeywordNotFound: true),
+                    pt = String.IsNullOrWhiteSpace(searchValue) ?
+                        c.post_text.TruncateTo(400) :
+                        truncateBySent(c.post_text, searchValue, sentPrepend: 0, sentAppend: 0, viewMoreTag: "[...]", startTruncTag: "[...]", returnSourceIfKeywordNotFound: true),
                     tt = c.translated_text,
                     lc = c.likes_count,
                     cc = c.comments_count,
@@ -3105,6 +3107,36 @@ namespace ArabicTextAnalyzer.Controllers
                     inners = inners.DefaultIfEmpty()
                 })
             .SelectMany(a => a.inners.Select(innerObj => resultSelector(a.outerObj, innerObj)));
+        }
+    }
+
+    public static class TruncateHelper
+    {
+        public static string TruncateTo(this string input, int truncateLength, bool addEllipsis = true)
+        {
+            // Check to see if we even need to bother truncating
+            if (string.IsNullOrWhiteSpace(input) || input.Length <= truncateLength)
+                return input;
+
+            // copy the input
+            var temp = input;
+
+            // Define a list of characters that we can safely break on
+            char[] anyOf = { '.', ',', ';', ':', '?', '!', '،', '؟' };
+
+            // LastIndexOfAny starts at the max position works backwards thru' the string
+            var truncatePosition = temp.LastIndexOfAny(anyOf, truncateLength);
+
+            // no appropriate place to truncate. 
+            if (truncatePosition == -1)
+            {
+                // Return original string but you could fall back to splitting on last space
+                return temp;
+            }
+            temp = temp.Substring(0, truncatePosition + (addEllipsis ? 0 : 1));
+            return addEllipsis
+                ? temp + "..."
+                : temp;
         }
     }
 
